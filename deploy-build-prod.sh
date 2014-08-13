15b2a82ad2b97e0204fcd23ac5de833d417ca73c
Phaneesh Nagaraja <phaneesh.n@gmail.com>
2014-08-13 09:21:03 +0530

    Enable imagemagick with lock to serialize calls (to avoid crash)

--- deploy-build-prod.sh
+++ deploy-build-prod.sh
@@ -7,12 +7,12 @@ do
     echo "Deployed build on "${API_HOST}
 done
 
-#for APINODE in 1 2 3 4
-#do
-#    API_HOST="rukmini"${APINODE}".nm.flipkart.com"
-#    echo "Deploying build on "${API_HOST}
-#    ssh ${API_HOST} "sudo apt-get update; sudo apt-get install rukmini"
-#    echo "Deployed build on "${API_HOST}
-#done
+for APINODE in 1 2 3 4
+do
+    API_HOST="rukmini"${APINODE}".nm.flipkart.com"
+    echo "Deploying build on "${API_HOST}
+    ssh ${API_HOST} "sudo apt-get update; sudo apt-get install rukmini"
+    echo "Deployed build on "${API_HOST}
+done
 echo "Done"
 echo
