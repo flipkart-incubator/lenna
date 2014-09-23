e458ce87adcd87994e39be2ab2b68dbcf9a893a4
Phaneesh Nagaraja <phaneesh.n@gmail.com>
2014-09-23 07:31:23 +0530

    Fix rolling restart script

--- rolling-restart.sh
+++ rolling-restart.sh
@@ -1,17 +1,24 @@
 #!/bin/bash -e
-#for APINODE in 2 3 4 5 12
-#do
-#    API_HOST="cdn-img"${APINODE}".nm.flipkart.com"
-#    echo "Deploying build on "${API_HOST}
-#    ssh ${API_HOST} "sudo /etc/init.d/rukmini stop; sudo /etc/init.d/rukmini start"
-#    echo "Restarted on "${API_HOST}
-#done
+for APINODE in 2 3 4 5 11 12
+do
+    API_HOST="cdn-img"${APINODE}".nm.flipkart.com"
+    echo "Restarting service on "${API_HOST}
+    ssh ${API_HOST} "sudo /etc/init.d/rukmini stop; sudo /etc/init.d/rukmini start"
+    echo "Restarted on "${API_HOST}
+done
 for APINODE in 1 2 3 4
 do
     API_HOST="rukmini"${APINODE}".nm.flipkart.com"
-    echo "Deploying build on "${API_HOST}
+    echo "Restarting service on "${API_HOST}
     ssh ${API_HOST} "sudo /etc/init.d/rukmini stop; sudo /etc/init.d/rukmini start"
     echo "Restarted on "${API_HOST}
+done
+for APINODE in 1 2 3 4 5 6 7 8
+do
+    API_HOST="mobile-rukmini"${APINODE}".nm.flipkart.com"
+    echo "Restarting service on "${API_HOST}
+    ssh ${API_HOST} "sudo /etc/init.d/rukmini stop; sudo /etc/init.d/rukmini start"
+    echo "Deployed build on "${API_HOST}
 done
 echo "Done"
 echo
