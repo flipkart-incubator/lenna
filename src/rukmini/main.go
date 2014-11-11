bce3b7e4230c969d6153154fe85d8284a5f2d2ec
Phaneesh Nagaraja <phaneesh.n@gmail.com>
2014-11-11 08:33:09 +0530

    Remove multiple header writes

--- src/rukmini/main.go
+++ src/rukmini/main.go
@@ -1,19 +1,17 @@
 package main
 
 import (
-	_ "rukmini/routers"
 	"github.com/astaxie/beego"
 	"net/http"
+	_ "rukmini/routers"
 )
 
 func rukmini_error_handler(rw http.ResponseWriter, r *http.Request) {
-	rw.WriteHeader(500)
 	rw.Write([]byte("Image delivery error"))
 }
 
 func main() {
 	beego.Errorhandler("500", rukmini_error_handler)
-	beego.SetLogger("file", `{"filename":"/var/log/rukmini/rukmini.log"}`)
+	beego.SetLogger("file", `{"filename":"/var/log/rukmini/rukmini.log", "daily" : true, "maxdays": 3, "rotate" : true, "maxsize" : 1000000000}`)
 	beego.Run()
 }
-
