335d8c70f6fcf2182de555a63db91b4a96c23c13
Phaneesh Nagaraja <phaneesh.n@gmail.com>
2014-11-12 09:49:37 +0530

    1) haproxy switch 2) Timeouts for server 3) Bad request error handler

--- src/rukmini/controllers/resize.go
+++ src/rukmini/controllers/resize.go
@@ -85,7 +85,7 @@ func (this *ResizeController) Get() {
 	fileName := fmt.Sprintf("/tmp/%s%s", u4, fileExt)
 	downloadUrl := fmt.Sprintf("%s%s", beego.AppConfig.String(resizeParameters.what+".source"), resizeParameters.uri)
 	req, _ := http.NewRequest("GET", downloadUrl, nil)
-	//	req.Host = "rukmini.flixcart.com"
+	req.Host = "rukmini.flixcart.com"
 	imageDownloadResponse, err := client.Do(req)
 	if err != nil {
 		logAccess(this, 500, 0)
--- src/rukmini/main.go
+++ src/rukmini/main.go
@@ -10,8 +10,13 @@ func rukmini_error_handler(rw http.ResponseWriter, r *http.Request) {
 	rw.Write([]byte("Image delivery error"))
 }
 
+func rukmini_bad_request_handler(rw http.ResponseWriter, r *http.Request) {
+	rw.Write([]byte("Bad request"))
+}
+
 func main() {
 	beego.Errorhandler("500", rukmini_error_handler)
+	beego.Errorhandler("400", rukmini_bad_request_handler)
 	beego.SetLogger("file", `{"filename":"/var/log/rukmini/rukmini.log", "daily" : true, "maxdays": 3, "rotate" : true, "maxsize" : 1000000000}`)
 	beego.Run()
 }
