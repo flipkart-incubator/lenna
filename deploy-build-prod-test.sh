a8127c47ca1166df07df92a2457d3ff77330d13a
Phaneesh Nagaraja <phaneesh.n@gmail.com>
2014-11-24 08:50:00 +0530

    1) Health check change 2) Test deploy script

---
+++ deploy-build-prod-test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+API_HOST="mobile-rukmini1.nm.flipkart.com"
+echo "Deploying build on "${API_HOST}
+ssh ${API_HOST} "sudo apt-get update; sudo apt-get install rukmini"
+echo "Deployed build on "${API_HOST}
+echo "Done"
+echo
