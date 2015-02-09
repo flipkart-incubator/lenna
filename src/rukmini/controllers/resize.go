6c5a08b6c67de13d6439f9b1a55826be972b186d
Pratyush Verma <pratyush.verma@flipkart.com>
2015-02-09 21:28:57 +0530

    added back rukmini in http get from cdn

--- src/rukmini/controllers/resize.go
+++ src/rukmini/controllers/resize.go
@@ -105,6 +105,7 @@ func (this *ResizeController) Get() {
 	fileName := fmt.Sprintf("/tmp/%s%s", u4, fileExt)
 	downloadUrl := fmt.Sprintf("%s%s", beego.AppConfig.String(resizeParameters.what+".source"), resizeParameters.uri)
 	req, _ := http.NewRequest("GET", downloadUrl, nil)
+	req.Host = "rukmini.flixcart.com"
 	imageDownloadResponse, err := client.Do(req)
 	if err != nil {
 		logAccess(this, 500, 0)
