9d4620395db9be8ae17588ad4228ab95e516aa6d
Pratyush Verma <pratyush.verma@flipkart.com>
2015-03-09 23:27:46 +0530

    minor script change

--- deploy-build-prod.sh
+++ deploy-build-prod.sh
@@ -3,14 +3,14 @@ for APINODE in 3 4 5 11 12
 do
     API_HOST="cdn-img"${APINODE}".nm.flipkart.com"
     echo "Deploying build on "${API_HOST}
-    ssh ${API_HOST} "sudo apt-get update; sudo apt-get install rukmini"
+    ssh ${API_HOST} "sudo apt-get update; sudo apt-get install rukmini=$1"
     echo "Deployed build on "${API_HOST}
 done
 for APINODE in 2 3 4 5 6 7 8
 do
     API_HOST="mobile-rukmini"${APINODE}".nm.flipkart.com"
     echo "Deploying build on "${API_HOST}
-    ssh ${API_HOST} "sudo apt-get update; sudo apt-get install rukmini"
+    ssh ${API_HOST} "sudo apt-get update; sudo apt-get install rukmini=$1"
     echo "Deployed build on "${API_HOST}
 done
 echo "Done"
